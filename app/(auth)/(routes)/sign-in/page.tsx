c1c065d1381c9b6592c06806b796bfc77827f1d1
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-12 11:35:47 +0200

    03: folders

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+  return ( 
+    <div>
+      This is an unprotected page
+    </div>
+   );
+}
+ 
+export default SignInPage;
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <p>This is a protected page</p>
+  )
+}
