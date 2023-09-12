ed4a7d6b76d634568b866158933a7c3ef7039e73
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-12 12:15:49 +0200

    04: auth

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
   return (
-    <p>This is a protected page</p>
+    <div>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   )
 }
