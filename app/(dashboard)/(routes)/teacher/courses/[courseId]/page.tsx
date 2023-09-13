ceb3da3967c00d9ef2e0604f3d208e8551c8dba2
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-13 21:37:44 +0200

    11: form title

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,8 @@ import { LayoutDashboard } from "lucide-react";
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 
+import { TitleForm } from "./_components/title-form";
+
 const CourseIdPage = async ({
   params
 }: {
@@ -59,6 +61,10 @@ const CourseIdPage = async ({
               Customize your course
             </h2>
           </div>
+          <TitleForm
+            initialData={course}
+            courseId={course.id}
+          />
         </div>
       </div>
     </div>
