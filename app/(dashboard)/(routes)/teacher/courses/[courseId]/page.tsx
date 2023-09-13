74cb2f22b4a2dc5f3abd04bdac11fe4eef1b0cb8
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-13 18:26:42 +0200

    09: create api

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,13 @@
+const CourseIdPage = ({
+  params
+}: {
+  params: { courseId: string }
+}) => {
+  return ( 
+    <div>
+      Course Id: {params.courseId}
+    </div>
+   );
+}
+ 
+export default CourseIdPage;
---
+++ app/api/courses/route.ts
@@ -0,0 +1,29 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function POST(
+  req: Request,
+) {
+  try {
+    const { userId } = auth();
+    const { title } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      }
+    });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSES]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
