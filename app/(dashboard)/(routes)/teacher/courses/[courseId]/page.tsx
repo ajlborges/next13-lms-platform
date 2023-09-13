3664d3edec6f13914c85bc4ddb63d19b5965a730
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-13 22:01:35 +0200

    12: description from

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({
   params
@@ -62,6 +63,10 @@ const CourseIdPage = async ({
             </h2>
           </div>
           <TitleForm
+            initialData={course}
+            courseId={course.id}
+          />
+          <DescriptionForm
             initialData={course}
             courseId={course.id}
           />
