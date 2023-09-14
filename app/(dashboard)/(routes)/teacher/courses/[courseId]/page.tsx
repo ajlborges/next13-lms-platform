28862045ee14f6d822f8f1281e732ae26f180042
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-14 12:21:09 +0200

    14: category form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -8,6 +8,7 @@ import { IconBadge } from "@/components/icon-badge";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({
   params
@@ -24,6 +25,12 @@ const CourseIdPage = async ({
     where: {
       id: params.courseId
     }
+  });
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
   });
 
   if (!course) {
@@ -74,6 +81,14 @@ const CourseIdPage = async ({
           <ImageForm
             initialData={course}
             courseId={course.id}
+          />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
           />
         </div>
       </div>
