94c170404d4841a949a23a6060001a8d60f81939
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-16 20:34:35 +0200

    28: search input

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/lib/db";
+import { SearchInput } from "@/components/search-input";
 
 import { Categories } from "./_components/categories";
 
@@ -9,12 +10,17 @@ const SearchPage = async () => {
     }
   });
 
-  return ( 
-    <div className="p-6">
-      <Categories
-        items={categories}
-      />
-    </div>
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories
+          items={categories}
+        />
+      </div>
+    </>
    );
 }
  
