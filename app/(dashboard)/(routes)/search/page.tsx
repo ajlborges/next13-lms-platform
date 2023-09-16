f453650baa6b26cad2f4705dec2f86288d031344
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-16 21:44:27 +0200

    29: course card

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,38 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 import { db } from "@/lib/db";
 import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { CoursesList } from "@/components/courses-list";
 
 import { Categories } from "./_components/categories";
 
-const SearchPage = async () => {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  }
+};
+
+const SearchPage = async ({
+  searchParams
+}: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc"
     }
+  });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
   });
 
   return (
@@ -15,10 +40,11 @@ const SearchPage = async () => {
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-4">
         <Categories
           items={categories}
         />
+        <CoursesList items={courses} />
       </div>
     </>
    );
