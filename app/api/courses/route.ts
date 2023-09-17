dee18bd1d4a4fa769c538d5a4d8d698101421f1f
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-17 22:41:21 +0200

    final

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
+import { isTeacher } from "@/lib/teacher";
 
 export async function POST(
   req: Request,
@@ -10,7 +11,7 @@ export async function POST(
     const { userId } = auth();
     const { title } = await req.json();
 
-    if (!userId) {
+    if (!userId || !isTeacher(userId)) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,11 +1,15 @@
 import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+import { isTeacher } from "@/lib/teacher";
  
 const f = createUploadthing();
  
 const handleAuth = () => {
   const { userId } = auth();
-  if (!userId) throw new Error("Unauthorized");
+  const isAuthorized = isTeacher(userId);
+
+  if (!userId || !isAuthorized) throw new Error("Unauthorized");
   return { userId };
 }
 
