0dede40ee9fd4127ada793189687997cb5241e97
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-17 12:22:04 +0200

    30: chapter layout

---
+++ app/(course)/courses/[courseId]/page.tsx
@@ -0,0 +1,9 @@
+const CourseIdPage = () => {
+  return ( 
+    <div>
+      Watch the course
+    </div>
+   );
+}
+ 
+export default CourseIdPage;
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -13,7 +13,7 @@ export const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
-  const isPlayerPage = pathname?.includes("/chapter");
+  const isCoursePage = pathname?.includes("/courses");
   const isSearchPage = pathname === "/search";
 
   return (
@@ -24,7 +24,7 @@ export const NavbarRoutes = () => {
         </div>
       )}
       <div className="flex gap-x-2 ml-auto">
-        {isTeacherPage || isPlayerPage ? (
+        {isTeacherPage || isCoursePage ? (
           <Link href="/">
             <Button size="sm" variant="ghost">
               <LogOut className="h-4 w-4 mr-2" />
