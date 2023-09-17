9f813a11863cc79bd872d041b59f4906284b37f2
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-09-17 22:08:40 +0200

    36: analytics

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,7 +1,40 @@
-const AnalyticsPage = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { getAnalytics } from "@/actions/get-analytics";
+
+import { DataCard } from "./_components/data-card";
+import { Chart } from "./_components/chart";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    data,
+    totalRevenue,
+    totalSales,
+  } = await getAnalytics(userId);
+
   return ( 
-    <div>
-      Analytics page
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard
+          label="Total Revenue"
+          value={totalRevenue}
+          shouldFormat
+        />
+        <DataCard
+          label="Total Sales"
+          value={totalSales}
+        />
+      </div>
+      <Chart
+        data={data}
+      />
     </div>
    );
 }
